33bf1a02fe81b0c273cebb187a31d310f26d4373
Nritya Kamath <nritya.k@gmail.com>
2019-05-26 16:06:34 -0700

    Assignment 3 complete

---
+++ index.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const fs = require('fs');
+const port = 3000;
+
+// Handles HTTP requests.
+const requestHandler = (request, response) => {
+  fs.writeFile('hello-world.txt', 'Hello to this great world', 'utf8', (err) => {
+  if (err) throw err;
+  console.log('success')
+  });
+  response.end(`Text file created. Running on Port: ${port}`)
+};
+
+// Create a server and pass in the  requestHandler function
+const server = http.createServer(requestHandler);
+
+// Start the server listening on port 8000
+server.listen(port, (err) => {
+  if (err) {
+    return console.log(`You have an error:  ${err}`);
+  }
+
+  console.log(`server is listening on ${port}`);
+});
